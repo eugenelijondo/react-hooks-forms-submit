550978f852f4c14d5b9883574f9ef2d4f58cb8e5
eugenelijondo <eugene.lijondo@student.moringaschool.com>
2024-02-10 08:59:49 +0300

    dont understand

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './Form'; // Assuming the Form component file is in the same directory
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Form</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
